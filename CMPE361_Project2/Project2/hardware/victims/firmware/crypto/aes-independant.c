c7d1a55f643853a3a33321adf29437b55787040e
Thoams <tcb2196@rit.edu>
2024-12-09 14:23:30 -0500

    Added red team

---
+++ CMPE361_Project2/Project2/hardware/victims/firmware/crypto/aes-independant.c
@@ -0,0 +1,32 @@
+/*
+    This file is part of the AESExplorer Example Targets
+    Copyright (C) 2012 Colin O'Flynn <coflynn@newae.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "aes-independant.h"
+#include "hal.h"
+
+
+uint8_t enckey[16];
+
+
+void aes_indep_key(uint8_t * key)
+{
+	for(uint8_t i=0; i < 16; i++){
+		enckey[i] = key[i];
+	}
+}
+
---
+++ CMPE361_Project2/Project2/hardware/victims/firmware/crypto/aes-independant.h
@@ -0,0 +1,30 @@
+/*
+    This file is part of the ChipWhisperer Example Targets
+    Copyright (C) 2012-2015 NewAE Technology Inc.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <stdint.h>
+
+#define KEY_LENGTH 16
+#define DEFAULT_KEY 0x2b,0x7e,0x15,0x16,0x28,0xae,0xd2,0xa6,0xab,0xf7,0x15,0x88,0x09,0xcf,0x4f,0x3c
+
+uint8_t extern enckey[16];
+
+
+void aes_indep_key(uint8_t * key);
+
+
